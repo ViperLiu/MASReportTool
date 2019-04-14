c4585be3d4c403bf611d3fb40e44812a9096c0ea
Unknown <a0960330@gmail.com>
2019-04-14 21:07:08 +0800

    remove unused using

--- MASReportTool/MainWindow.xaml.cs
+++ MASReportTool/MainWindow.xaml.cs
@@ -1,24 +1,11 @@
-﻿using Microsoft.Win32;
-using Newtonsoft.Json;
+using Microsoft.Win32;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Windows.Threading;
 
 namespace MASReportTool
 {
