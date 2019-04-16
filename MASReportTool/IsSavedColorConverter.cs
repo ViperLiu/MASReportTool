e4ce5799dc1d50e7fec3d68b1f5cedf35ff28c7e
Unknown <a0960330@gmail.com>
2019-04-16 17:37:09 +0800

    converter

---
+++ MASReportTool/IsSavedColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MASReportTool
+{
+    class IsSavedColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(bool)value)
+                return "#FF6666";
+            else
+                return "#FF333333";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
