e4d9e6e608faf1d09e4ec97fee8bd895a55b7e39
Unknown <a0960330@gmail.com>
2019-04-17 17:46:45 +0800

    ResultStringIconConverter
    將檢測結果轉換成對應的圖片路徑

---
+++ MASReportTool/ResultStringIconConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MASReportTool
+{
+    class ResultStringIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (value.ToString().ToLower())
+            {
+                case "accept":
+                    return "/assets/pics/icon_accept.png";
+                case "fail":
+                    return "/assets/pics/icon_fail.png";
+                case "notfit":
+                    return "/assets/pics/icon_notfit.png";
+            }
+            return Binding.DoNothing;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
