24420eac47b157474c22fee8d7e7ec6666518fb9
Unknown <a0960330@gmail.com>
2019-04-23 13:46:54 +0800

    FunctionBarViewModel & RelayCommand

---
+++ MASReportTool/FunctionBarViewModel.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MASReportTool
+{
+    class FunctionBarViewModel
+    { 
+        public Report Report { get; }
+
+        public FunctionBarViewModel(Report report)
+        {
+            this.Report = report;
+        }
+
+        public ICommand SaveFile
+        {
+            get { return new RelayCommand(SaveFileExecute, CanSaveFileExecute); }
+        }
+
+        void SaveFileExecute()
+        {
+            Console.WriteLine(Report.CurrentOpenedFile);
+            //如果檔案存在就直接存檔
+            if (File.Exists(Report.CurrentOpenedFile))
+            {
+                Save(Report.CurrentOpenedFile);
+                return;
+            }
+
+            //檔案不存在就開啟存檔視窗
+            SaveAsNewFile();
+        }
+        bool CanSaveFileExecute()
+        {
+            return true;
+        }
+        private void Save(string file)
+        {
+            Console.WriteLine("saved : " + file);
+            JsonFileController json = new JsonFileController(file);
+            json.SaveFile(Report);
+            Report.MarkAsSaved();
+            Report.CurrentOpenedFile = file;
+        }
+        private void SaveAsNewFile()
+        {
+            SaveFileDialog saveJsonrDialog = new SaveFileDialog();
+            saveJsonrDialog.Filter = "MAS報告|*.jsonr";
+            saveJsonrDialog.DefaultExt = ".jsonr";
+            saveJsonrDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            var result = saveJsonrDialog.ShowDialog();
+            var file = saveJsonrDialog.FileName;
+            if (result == true)
+            {
+                Save(file);
+            }
+        }
+    }
+}
---
+++ MASReportTool/RelayCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MASReportTool
+{
+    internal class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        public RelayCommand(Action saveFileExecute, Func<bool> canSaveFileExecute)
+        {
+            _execute = saveFileExecute;
+            _canExecute = canSaveFileExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+
+                if (_canExecute != null)
+                    CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+
+                if (_canExecute != null)
+                    CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+    }
+}
