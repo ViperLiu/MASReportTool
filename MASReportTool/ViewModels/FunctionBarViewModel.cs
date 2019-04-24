489b35e8261cf2fa1903d0a1e5d8e892da5e229f
Unknown <a0960330@gmail.com>
2019-04-24 10:17:00 +0800

    namespace

--- MASReportTool/ViewModels/FunctionBarViewModel.cs
+++ MASReportTool/ViewModels/FunctionBarViewModel.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace MASReportTool
+namespace MASReportTool.ViewModels
 {
     class FunctionBarViewModel
     { 
--- MASReportTool/ViewModels/TreeViewItemsViewModel.cs
+++ MASReportTool/ViewModels/TreeViewItemsViewModel.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MASReportTool
+namespace MASReportTool.ViewModels
 {
     class TreeViewItemsViewModel
     {
