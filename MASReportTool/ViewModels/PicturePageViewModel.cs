afff78ac0dd6a5c768a543e35e96311e9ad0e432
Unknown <a0960330@gmail.com>
2019-04-24 14:10:46 +0800

    picture view model

---
+++ MASReportTool/ViewModels/PicturePageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MASReportTool.ViewModels
+{
+    class PicturePageViewModel
+    {
+        public SubRuleResult SubRule { get; }
+
+        public Picture CurrentSelectedPic { get; }
+
+        public List<PictureViewModel> Pictures { get; }
+
+        public PicturePageViewModel(SubRuleResult subRule)
+        {
+            SubRule = subRule;
+            Pictures = new List<PictureViewModel>();
+            foreach(var pic in subRule.Pictures)
+            {
+                Pictures.Add(new PictureViewModel(pic));
+            }
+        }
+
+        
+    }
+}
