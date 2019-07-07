1ba92d127b9ccae7672f6e8acb69ae385a4566a5
ViperLiu <a0960330@gmail.com>
2019-07-07 15:14:29 +0800

    更新 ValueConverter

--- MASReportTool/ValueConverters/ResultStringConverter.cs
+++ MASReportTool/ValueConverters/ResultStringConverter.cs
@@ -12,7 +12,18 @@ namespace MASReportTool.ValueConverters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return (value as ITestResult).GetDisplayString();
+            switch (value.ToString().ToLower())
+            {
+                case "accept":
+                    return "符合";
+                case "fail":
+                    return "不符合";
+                case "notfit":
+                    return "不適用";
+                case "undetermin":
+                    return "未檢測";
+            }
+            return "ERROR";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
